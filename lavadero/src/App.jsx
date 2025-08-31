aa93242a8c6604e4533db3ab9a3639f11c819e39
chavow5 <davidramirezw5@gmail.com>
2025-08-31 11:48:13 -0300

    Cambios de imagen

--- lavadero/src/App.jsx
+++ lavadero/src/App.jsx
@@ -9,10 +9,15 @@ function App() {
   return (
     <div
       className="min-h-screen w-full bg-cover bg-center relative"
-      style={{ backgroundImage: "url('/img/fondolavado.png')" }}
+      style={{
+        backgroundImage: "url('/img/fondolavado.jpg')",
+        backgroundRepeat: 'repeat',      // repite la imagen
+        backgroundSize: '1320px',         // tamaño del tile (ajustalo)
+        backgroundPosition: 'center'
+      }}
     >
       {/* Overlay oscuro en toda la página */}
-      <div className="absolute inset-0 bg-black/70"></div>
+      <div className="absolute inset-0 bg-black/60"></div>
 
       {/* Contenido encima del overlay */}
       <div className="relative z-10">
